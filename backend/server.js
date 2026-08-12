3fe54092ed1c4df2c79db844465f3ff9c5e3cb95
shivamkumar <kshivam14566@gmail.com>
2026-08-12 20:07:59 +0530

    fixed production api connection

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin:[
+    "https://blog-application-pzqyvynsb-shivam-blog-application.vercel.app"
+    ]
+}));
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
