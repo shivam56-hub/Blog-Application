0e944fcaf8c1e5d6d182ec53ad68e349df831fd2
shivamkumar <kshivam14566@gmail.com>
2026-08-13 01:57:22 +0530

    cloudinary change

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -4,6 +4,13 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
+    
+});
+console.log("Cloudinary config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY ? "SET" : "MISSING",
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "SET" : "MISSING"
 });
 
 module.exports = cloudinary;
+
